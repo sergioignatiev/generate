66a70b4c3957cbe45b38c3611f3de52974dd43f5
SergioIgnatev <ignatevsergey17@gmail.com>
2024-05-28 00:17:51 +0300

    Updates

--- assets/ClDO6HV2.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,j as o,o as c}from"./CuN61e-k.js";const r={},a={class:"py-10"};function n(e,_){return c(),t("div",a,[o(e.$slots,"default")])}const d=s(r,[["render",n]]);export{d as default};
